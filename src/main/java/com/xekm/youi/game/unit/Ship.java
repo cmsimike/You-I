05cec268311d0384ff9e8b0977a52c3c6458beef
Mike Megally <cmsimike@gmail.com>
2014-05-07 23:11:06 -0700

    quick notes to myself as i think about thrust-based ships

--- src/main/java/com/xekm/youi/game/unit/Ship.java
+++ src/main/java/com/xekm/youi/game/unit/Ship.java
@@ -6,11 +6,21 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public class Ship extends BaseUnit {
-
+  private int thrust;
   public Ship() {
     super(Color.darkGray, 1, 1);
   }
 
+  //required - 2d vector movement?
+  // thrust which leads to accel
+//http://stackoverflow.com/questions/5705397/java-movement-in-2d
+  // ships have a certain acceleration (km/h/h)
+  // which we then keep track of the velocity.
+  // acceleration is based on engine type.
+  // 2500 ms = 1 hour
+  // 60 seconds = 1 day.
+  // TODO may want ot increase this?
+  // so 600 orbital days = 600 minutes
   @Override
   public void update(long millis) {
 
